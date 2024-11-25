0da0f91bf2aa2dc8e440f7ab5c9f9adf36dafed3
Sharked <103890697+SharkedUwu@users.noreply.github.com>
2024-11-25 08:57:51 -0300

    Test Agregados

--- backend/test/sample.test.ts
+++ backend/test/sample.test.ts
@@ -1,6 +1,23 @@
 import prisma from "@/lib/db";
 import { obtainUser } from "../auth/sign-in";
 import signIn from "../auth/sign-in";
+import { compareHash } from "../../lib/crypto";
+
+const existingUser = {
+  id: "user-id-123",
+  email: "hello@prisma.io",
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  name: "Manzano Malo",
+  hashedPassword: "hashed-password",
+};
+
+const expectedError = {
+  success: false,
+  data: null,
+  errorVariables: ["email", "password"],
+  message: "Credenciales inválidas",
+};
 
 describe("Pruebas con prisma mockeado", () => {
   it("Debería llamar a user.findMany", async () => {
@@ -10,52 +27,46 @@ describe("Pruebas con prisma mockeado", () => {
       { id: 2, name: "Usuario 2" },
     ]);
 
-    // Llamar al método
     const users = await prisma.user.findMany();
 
-    // Verificar el resultado
     expect(users).toEqual([
       { id: 1, name: "Usuario 1" },
       { id: 2, name: "Usuario 2" },
     ]);
 
-    // Verificar que el método fue llamado
     expect(prisma.user.findMany).toHaveBeenCalledTimes(1);
   });
 
   it("Debería retornar usuario mockeado", async () => {
-    // Configurar el mock para que devuelva datos simulados
-    const existingUser = {
-      id: "user-id-123",
-      email: "hello@prisma.io",
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      name: "Manzano Malo",
-      hashedPassword: "hashed-password",
-    };
-
     (prisma.user.findUnique as jest.Mock).mockResolvedValue(existingUser);
     const result = await obtainUser("hello@prisma.io");
-    console.log(result);
+    expect(prisma.user.findUnique).toHaveBeenCalledWith({
+      where: { email: "hello@prisma.io" },
+    });
   });
 
-  it("Debería retornar error", async () => {
+  it("Debería retornar error, ususario no encontrado", async () => {
     (prisma.user.findUnique as jest.Mock).mockResolvedValue(null);
 
-    const expectedError = {
-      success: false,
-      data: null,
-      errorVariables: ["email", "password"],
-      message: "Credenciales inválidas",
-    };
-
-    // Llama a signIn y verifica la respuesta
     const result = await signIn({
       email: "invalid@prisma.io",
       password: "*NotPassword123",
     });
 
-    // Verifica que la respuesta sea igual al error esperado
+    expect(result).toEqual(expectedError);
+  });
+
+  it("Debería retornar error, contraseña incorrecta", async () => {
+    jest.mock("@/lib/crypto", () => ({
+      compareHash: jest.fn(() => false), // Simula que la contraseña es correcta
+    }));
+
+    (prisma.user.findUnique as jest.Mock).mockResolvedValue(existingUser);
+    //Email y password dan igual, debido a que findUnique retornará existingUser
+    const result = await signIn({
+      email: "a",
+      password: "b",
+    });
     expect(result).toEqual(expectedError);
   });
 });
